5c4da7c81a570e8007641535abdfca2bd1f516fc
Jung-gh <70095017+Jung-gh@users.noreply.github.com>
2022-02-13 17:15:28 +0900

    Personal Info

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+
+const { User } = require("./models/User");
+
+//각 타입으로 된 것을 분석해서 가져올 수 있도록 함
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://eli:1234@boilerplate.gn1th.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! This is the Test!')
+})
+
+app.post('/register', (req, res) => {
+
+  //fetch the info which needed for sing up and send it to database
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
 })
 
 app.listen(port, () => {
